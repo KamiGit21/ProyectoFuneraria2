4913d46cd201f85999e31440767c1fdf5b1432d8
CaFePiMe <159438510+CaFePiMe@users.noreply.github.com>
2025-04-16 00:17:02 -0400

    home

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -3,13 +3,24 @@ import { Container, Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <Container maxWidth="md" sx={{ mt: 10 }}>
-      <Typography variant="h2" gutterBottom>
-        Bienvenido a LumenGest
-      </Typography>
-      <Typography variant="body1">
-        Esta es la página principal (Home). ¡Prueba que la navegación funciona correctamente!
-      </Typography>
+    <Container>
+      <Container style={{ display: 'flex', width: '100%' }}>
+        <Container style={{ flex: 1 }}>
+          <img src="src/Imagen.png" alt="Imagen" style={{ width: '100%', height: 'auto' }} />
+        </Container>
+        <Container style={{ flex: 1, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+          <Typography variant="h4">Bienvenido a LumenGest</Typography>
+        </Container>
+      </Container>
+      <Container style={{ display: 'flex', width: '100%', marginTop: '20px' }}>
+        <Container style={{ flex: 1, textAlign: 'center' }}>
+          <Typography variant="h5">Quiénes somos</Typography>
+        </Container>
+        <Container style={{ flex: 1, textAlign: 'center' }}>
+          <Typography variant="h5">Testimonios</Typography>
+        </Container>
+      </Container>
     </Container>
   );
 }
+//Bienvenido a LumenGest
