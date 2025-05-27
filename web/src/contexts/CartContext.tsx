99203c9e606fb66469686bc0b2ec9946aa920711
EduardM173 <eduardo.apaza@ucb.edu.bo>
2025-05-27 17:15:42 -0400

    Resuelto merge entre main y Servicios-Checkout

--- web/src/contexts/CartContext.tsx
+++ web/src/contexts/CartContext.tsx
@@ -67,8 +67,4 @@ export const useCart = () => {
   const ctx = useContext(CartContext);
   if (!ctx) throw new Error('useCart debe usarse dentro de <CartProvider>');
   return ctx;
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> main
